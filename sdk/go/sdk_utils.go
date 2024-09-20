9eee9f71a4f136c3d93e1019daf0fe57e5b5cf19
Clint J Edwards <clint.j.edwards@gmail.com>
2024-09-20 13:28:26 -0400

    feat(sdk): Add extension list subscription convenience function

--- sdk/go/sdk_utils.go
+++ sdk/go/sdk_utils.go
@@ -2,7 +2,9 @@ package sdk
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
+	"io"
 	"net/http"
 
 	"github.com/oapi-codegen/oapi-codegen/v2/pkg/securityprovider"
@@ -21,17 +23,54 @@ func (g GoferAPIVersion) goferAPIVersionInterceptor(_ context.Context, req *http
 }
 
 // Creates a new client for the main Gofer API with all the required headers.
-func NewClientWithHeaders(host, token string, apiVersion GoferAPIVersion) (*Client, error) {
+func NewClientWithHeaders(host, token string, useTLS bool, apiVersion GoferAPIVersion) (*Client, error) {
+	scheme := "http://"
+
+	if useTLS {
+		scheme = "https://"
+	}
+
 	auth, err := securityprovider.NewSecurityProviderBearerToken(token)
 	if err != nil {
 		return nil, fmt.Errorf("could not establish Gofer client while attempting to create auth header: %w", err)
 	}
 
-	client, err := NewClient(host, WithRequestEditorFn(auth.Intercept),
+	client, err := NewClient(scheme+host, WithRequestEditorFn(auth.Intercept),
 		WithRequestEditorFn(apiVersion.goferAPIVersionInterceptor))
 	if err != nil {
 		return nil, fmt.Errorf("could not establish Gofer client: %w", err)
 	}
 
 	return client, nil
+}
+
+// List extension specific subscriptions.
+func ListExtensionSubscriptions(extension_id, goferHost, secret string, useTLS bool, apiVersion GoferAPIVersion) ([]Subscription, error) {
+	client, err := NewClientWithHeaders(goferHost, secret, useTLS, apiVersion)
+	if err != nil {
+		return nil, fmt.Errorf("could not establish Gofer client: %w", err)
+	}
+
+	resp, err := client.ListExtensionSubscriptions(context.Background(), extension_id)
+	if err != nil {
+		return nil, fmt.Errorf("could not query Gofer for extension subscriptions")
+	}
+
+	defer resp.Body.Close()
+
+	if resp.StatusCode < 200 || resp.StatusCode > 299 {
+		return nil, fmt.Errorf("could not query Gofer for extension subscriptions; status_code: %d;", resp.StatusCode)
+	}
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, fmt.Errorf("could not read response body while attempting to query for extension subscriptions")
+	}
+
+	listExtensionSubscriptionsResponse := ListExtensionSubscriptionsResponse{}
+	if err := json.Unmarshal(body, &listExtensionSubscriptionsResponse); err != nil {
+		return nil, fmt.Errorf("could not parse response body while attempting to query for extension subscriptions")
+	}
+
+	return listExtensionSubscriptionsResponse.Subscriptions, nil
 }
