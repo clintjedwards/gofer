04c999cca4173ffa5c0f4001c585814c67895240
Clint J Edwards <clint.j.edwards@gmail.com>
2025-01-27 00:57:30 -0500

    chore: update run-tests to more robust caching script

--- dogfood/run_tests/src/main.rs
+++ dogfood/run_tests/src/main.rs
@@ -7,33 +7,40 @@ fn main() {
             "run-cargo-test",
             "ghcr.io/clintjedwards/gofer/tools:rust",
         )
-        .description("Run cargo test command for workspace")
-        .always_pull_newest_image(true)
-        // We need to insert the gofer api base url so that the gofer CLI knows where to send requests.
-        .variables(vec![("GOFER_API_BASE_URL", "http://172.17.0.1:8080")])
-        .script(
-            r#"
-            set -euxo pipefail
+            .description("Run cargo test command for workspace")
+            .always_pull_newest_image(true)
+            .inject_api_token(true)
+            // We need to insert the gofer api base url so that the gofer CLI knows where to send requests.
+            .variables(vec![("GOFER_API_BASE_URL", "http://172.17.0.1:8080")])
+            .script(
+                r#"
+                set -euxo pipefail
 
-            # If this isn't set by the Github extension then set it ourselves to the main branch
-            GOFER_EXTENSION_GITHUB_PULLREQUEST_BRANCH="${GOFER_EXTENSION_GITHUB_PULLREQUEST_BRANCH:-main}"
+                # If this isn't set by the Github extension then set it ourselves to the main branch
+                GOFER_EXTENSION_GITHUB_PULLREQUEST_BRANCH="${GOFER_EXTENSION_GITHUB_PULLREQUEST_BRANCH:-main}"
             
-            git clone --depth 1 --branch "$GOFER_EXTENSION_GITHUB_PULLREQUEST_BRANCH" https://github.com/clintjedwards/gofer --single-branch
+                git clone --depth 1 --branch "$GOFER_EXTENSION_GITHUB_PULLREQUEST_BRANCH" https://github.com/clintjedwards/gofer --single-branch
 
-            cd gofer
+                cd gofer
 
-            gofer pipeline object get run-tests cache > /tmp/rust_cache.tar.gz
-            tar -xzf /tmp/rust_cache.tar.gz
-            mv .cargo ~/.cargo
+                if gofer pipeline object get run-tests cache > /tmp/rust_cache.tar.gz; then
+                    if tar -xzf /tmp/rust_cache.tar.gz; then
+                        mv .cargo ~/.cargo
+                    else
+                        echo "Cache extraction failed, proceeding without cache."
+                    fi
+                else
+                    echo "Cache fetch failed, proceeding without cache."
+                fi
 
-            cargo test
+                cargo test
 
-            tar -czf /tmp/rust_cache.tar.gz target ~/.cargo
+                tar -czf /tmp/rust_cache.tar.gz target ~/.cargo
 
-            gofer pipeline object put run-tests --force cache /tmp/rust_cache.tar.gz
+                gofer pipeline object put run-tests --force cache /tmp/rust_cache.tar.gz
 
-            "#,
-        )])
+                "#,
+            )])
         .finish()
         .unwrap();
 }
